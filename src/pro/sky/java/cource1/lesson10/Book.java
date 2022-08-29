631234928871ef7556396be8b322ede5e7146ca3
MonakhovA <MonGraph67>
2022-08-29 20:20:39 +0300

    Домашнее задание 1.10

--- src/pro/sky/java/cource1/lesson10/Book.java
+++ src/pro/sky/java/cource1/lesson10/Book.java
@@ -1,4 +1,4 @@
-package pro.sky.java.cource1.lesson9;
+package pro.sky.java.cource1.lesson10;
 
 public class Book {
     private String title;
@@ -25,4 +25,19 @@ public class Book {
         this.publicationYear=publicationYear;
     }
 
+    @Override
+    public String toString(){
+        return "Название книги "+title+", Автор "+author+", Год публикации "+publicationYear;
+    }
+
+    public boolean equals(Book anotherBook){
+        return anotherBook.title.equals(this.title)
+                && anotherBook.author.equals(this.author)
+                && anotherBook.publicationYear==this.publicationYear;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(title+author+publicationYear);
+    }
 }
